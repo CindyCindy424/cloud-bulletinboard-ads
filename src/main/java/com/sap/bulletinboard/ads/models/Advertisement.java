e539a02f3b01ae25e002343821b5c58dc96123c0
Cindy Deng <cindy.deng01@sap.com>
2022-02-24 13:13:59 +0800

    finish exercise 8-1

--- src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
+++ src/main/java/com/sap/bulletinboard/ads/models/Advertisement.java
@@ -1,8 +1,18 @@
 package com.sap.bulletinboard.ads.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.hibernate.validator.constraints.NotBlank;
 
+@Entity
 public class Advertisement {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotBlank
     private String title;
